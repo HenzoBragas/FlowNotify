096513846cf5f5a6f9c14ac8fca152e53ef27ef3
Henzo Bragas <henzo.bragas@hotmail.com>
2025-09-07 11:56:59 -0300

    feat: configuração do serviço de comunicação do kafka producer

--- servicocompra/src/main/java/com/servicocompra/service/OrderService.java
+++ servicocompra/src/main/java/com/servicocompra/service/OrderService.java
@@ -2,13 +2,16 @@ package com.servicocompra.service;
 
 
 import com.servicocompra.dto.OrderDTO;
+import com.servicocompra.event.OrderSaveEvent;
 import com.servicocompra.model.Order;
 import com.servicocompra.repository.OrderRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.messaging.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.function.StreamBridge;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -28,9 +31,22 @@ public class OrderService {
     @Transactional
     public OrderDTO serviceSubmit(OrderDTO order) {
 
+        log.info("Recebendo requisição");
+
         Order request = new Order(order);
 
         Order requestSave = orderRepository.save(request);
+
+        OrderSaveEvent event = new OrderSaveEvent(requestSave);
+
+        Message<OrderSaveEvent> message = MessageBuilder
+                .withPayload(event)
+                .setHeader(KafkaHeaders.KEY, String.valueOf(event.id_buy()))
+                .build();
+
+        log.info("Enviando evento para Kafka com a chave: {}", event.id_buy());
+        streamBridge.send("enviarPedido-out-0", message);
+        log.info("Evento enviado com sucesso");
 
         return new OrderDTO(requestSave);
     }
